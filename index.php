597a080414fb4f3614adffad8b732c3115c428b7
iltommi1995 <tomasdanielavilavisintin@yahoo.it>
2020-02-13 19:15:07 +0100

    tutte pagine

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php include_once("home.html"); ?>
 <?php include_once("Animale.html"); ?>
+<?php include_once("Animale_descrizione.html"); ?>
+<?php include_once("Animale_Habitat_e_dieta.html"); ?>
+<?php include_once("Animale_Riproduzione.html"); ?>
+<?php include_once("Curiosita.html"); ?>
+<?php include_once("Minacce.html"); ?>
