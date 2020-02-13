6a8e4aacc768648fc12a35e94756d10df5cbe84a
iltommi1995 <tomasdanielavilavisintin@yahoo.it>
2020-02-13 19:22:28 +0100

    animale descrizione

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include_once("home.html"); ?>
 <?php include_once("Animale.html"); ?>
-<?php include_once("Animale_Descrizione.html"); ?>
+<?php include_once("Animale_descrizione.html"); ?>
 <?php include_once("Animale_Habitat_e_dieta.html"); ?>
 <?php include_once("Animale_Riproduzione.html"); ?>
 <?php include_once("Curiosita.html"); ?>
