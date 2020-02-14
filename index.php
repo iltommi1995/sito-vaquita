27e5a2035496fdfa264e696e0901f409f6e4d61f
Tomàs Avila <59200709+iltommi1995@users.noreply.github.com>
2020-02-14 14:49:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php include_once("home.html"); ?>
-<?php include_once("Animale.html"); ?>
 <?php include_once("Animale_Descrizione.html"); ?>
 <?php include_once("Animale_Habitat_e_dieta.html"); ?>
 <?php include_once("Animale_Riproduzione.html"); ?>
